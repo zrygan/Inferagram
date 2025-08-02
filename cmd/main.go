8fff1959dbfabf002e8162c2de0814cf69857bf8
Zhean Ganituen <zr.gntn@gmail.com>
2025-08-02 03:38:30 -0400

    Refactor main function to use ReadFromFile and implement sorting and abstraction methods

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,10 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Inferagram ~\t Corpus filename is not provided")
 	} else if len(os.Args) == 2 {
-		c := corpus.ReadCorpus(os.Args[1])
-		c.PrintCorpus()
+		c := corpus.ReadFromFile(os.Args[1])
+		c.Sort()
+		ac := c.PositionalAbstraction()
+		ac.Print()
 	} else {
 		fmt.Println("Inferagram ~\t More than one corpus provided")
 	}
